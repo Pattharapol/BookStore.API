9ee69a8a9e6aae4b30e69802dab7809ee53cbe8e
Pattharapol Lakboon <tjtik83@gmail.com>
2022-02-15 19:49:50 +0700

    project starter

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿
+using BookStore.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var books = await _bookRepository.GetAllBooksAsync();
+
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetBookByID([FromRoute] int id)
+        {
+            var book = await _bookRepository.GetBookByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+    }
+}
---
+++ BookStore.API/Repository/BookRepository.cs
@@ -0,0 +1,41 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookStoreContext context;
+        public BookRepository(BookStoreContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<BookModel>> GetAllBooksAsync()
+        {
+            var records = await this.context.Books.Select(x => new BookModel()
+            {
+                Id = x.Id,
+                Description = x.Description,
+                Title = x.Title,
+            }).ToListAsync();
+
+            return records;
+        }
+
+        public async Task<BookModel> GetBookByIdAsync(int bookId)
+        {
+            var records = await this.context.Books.Where(x => x.Id == bookId).Select(x => new BookModel()
+            {
+                Id = x.Id,
+                Description = x.Description,
+                Title = x.Title,
+            }).FirstOrDefaultAsync();
+
+            return records;
+        }
+    }
+}
